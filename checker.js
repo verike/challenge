f3c435e7aaa3ed4ed846d4982a350c1b66518472
Victor Erike <verike7@gmail.com>
2022-12-01 20:45:47 +0100

    Removed comments

--- checker.js
+++ checker.js
@@ -1,28 +1,3 @@
-// Declaring the function 
-// function Solution(first_str,secound_str)
-// {
-        // checking for match, starting from end of second string as reference
-//     feedback = first_str.substring(-secound_str.length);
-
-        // comparing results of extracted sub string
-//     if (feedback == secound_str)
-//     {
-//         return true;
-//     }
-//     else
-//     {
-//         return false;
-//     }
-// }
-
-// Initializing function
-
-// result = Solution('learnable', 'able');
-// result2 = Solution('learnable', 'bled');
-
-// console.log(result);
-// console.log(result2);
-
 // Declaring function
 function solution(first_word, second_word)
 {
